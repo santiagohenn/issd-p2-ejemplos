f067216eea145f78fa95f6cf8243bb871bb5c2ce
Santi <santiagohenn@gmail.com>
2025-09-04 23:21:34 +0200

    added Clase01

---
+++ Clase01/Clase1.java
@@ -0,0 +1,17 @@
+class Clase1 {
+    public static void main(String[] args) {
+        int num = 2;
+        double num2 = 3.5;
+        String nombre = "Juan";
+
+        while (num < 10) {
+            print("Hola mundo");
+            num++;
+        }
+    }
+
+    public static void print(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+}
---
+++ Clase01/Test.java
@@ -0,0 +1,12 @@
+class Test {
+    
+    int a = 12;
+    int b = 5;
+
+    if (a >= b) {
+        System.out.println("a es igual a b");
+    } else {
+        System.out.println("a no es igual a b");
+    }
+
+}
